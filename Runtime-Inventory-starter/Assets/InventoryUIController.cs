f544f72176ba3993a92d7e90216ec4eab9ef9f87
DragonAngel1st <31282296+DragonAngel1st@users.noreply.github.com>
2022-06-23 18:00:51 -0400

    wip

--- Runtime-Inventory-starter/Assets/InventoryUIController.cs
+++ Runtime-Inventory-starter/Assets/InventoryUIController.cs
@@ -6,9 +6,14 @@ using UnityEngine.UIElements;
 
 public class InventoryUIController : MonoBehaviour
 {
+    private static VisualElement m_GhostIcon;
     public List<InventorySlot> InventoryItems = new List<InventorySlot>();
     private VisualElement m_Root;
     private VisualElement m_SlotContainer;
+
+    //Drag and Drop vars
+    private static bool m_IsDragging;
+    private static InventorySlot m_OriginalSlot;
 
     private void Awake()
     {
@@ -17,7 +22,7 @@ public class InventoryUIController : MonoBehaviour
         //Search the root for the SlotContainer Visual Element
         m_SlotContainer = m_Root.Q<VisualElement>("SlotContainer");
         //Create InventorySlots and add them as children to the SlotContainer
-        for (int i = 0; i < 20; i++)
+        for (int i = 0; i < 48; i++)
         {
             
             InventorySlot item = new InventorySlot();
@@ -28,6 +33,55 @@ public class InventoryUIController : MonoBehaviour
 
         GameController.OnInventoryChanged += GameController_OnInventoryChanged;
 
+        m_GhostIcon = m_Root.Query<VisualElement>("GhostIcon");
+
+        m_GhostIcon.RegisterCallback<PointerMoveEvent>(OnPointerMove);
+        m_GhostIcon.RegisterCallback<PointerUpEvent>(OnPointerUp);
+    }
+
+    private void OnPointerUp(PointerUpEvent evt)
+    {
+        if (!m_IsDragging)
+        {
+            return;
+        }
+        //Check to see if they are dropping the ghost icon over any inventory slots.
+        IEnumerable<InventorySlot> slots = InventoryItems.Where(x =>
+               x.worldBound.Overlaps(m_GhostIcon.worldBound));
+        //Found at least one
+        if (slots.Count() != 0)
+        {
+            InventorySlot closestSlot = slots.OrderBy(x => Vector2.Distance
+               (x.worldBound.position, m_GhostIcon.worldBound.position)).First();
+
+            //Set the new inventory slot with the data
+            closestSlot.HoldItem(GameController.GetItemByGuid(m_OriginalSlot.ItemGuid));
+
+            //Clear the original slot
+            m_OriginalSlot.DropItem();
+        }
+        //Didn't find any (dragged off the window)
+        else
+        {
+            m_OriginalSlot.Icon.image =
+                  GameController.GetItemByGuid(m_OriginalSlot.ItemGuid).Icon.texture;
+        }
+        //Clear dragging related visuals and data
+        m_IsDragging = false;
+        m_OriginalSlot = null;
+        m_GhostIcon.style.visibility = Visibility.Hidden;
+    }
+
+    private void OnPointerMove(PointerMoveEvent evt)
+    {
+        //Only take action if the player is dragging an item around the screen
+        if (!m_IsDragging)
+        {
+            return;
+        }
+        //Set the new position
+        m_GhostIcon.style.top = evt.position.y - m_GhostIcon.layout.height / 2;
+        m_GhostIcon.style.left = evt.position.x - m_GhostIcon.layout.width / 2;
     }
 
     private void GameController_OnInventoryChanged(string[] itemGuid, InventoryChangeType change)
@@ -46,4 +100,23 @@ public class InventoryUIController : MonoBehaviour
             }
         }
     }
+
+    public static void StartDrag(Vector2 position, InventorySlot originalSlot)
+    {
+        //Set tracking variables
+        m_IsDragging = true;
+        m_OriginalSlot = originalSlot;
+
+        //Set new position
+        m_GhostIcon.style.top = position.y - m_GhostIcon.layout.height / 2;
+        m_GhostIcon.style.left = position.x - m_GhostIcon.layout.width / 2;
+
+        //Set the image
+        m_GhostIcon.style.backgroundImage = GameController.GetItemByGuid(originalSlot.ItemGuid).Icon.texture;
+
+        //Fli the visibility
+        m_GhostIcon.style.visibility = Visibility.Visible;
+    }
+
+
 }
